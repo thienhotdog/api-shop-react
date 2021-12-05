32a557104d62eef2efafe45e926f40a8bbf45ae5
QuangHieu <thienhotdog@gmail.com>
2021-12-06 02:10:06 +0700

    update

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { add, getAll } from '../control/order';
+import { add, get, getAll } from '../control/order';
 const router = express.Router();
 
 router.post("/order", add);
 
-router.get("/orders", getAll)
+router.get("/orders", getAll);
+
+router.get("/order/:id", get)
 
 module.exports = router;
